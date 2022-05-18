7a3d624a2ffadbf227287b784b2192c8e1eeea7b
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-05-17 19:56:52 -0500

    implemented schedule entity

--- src/main/java/com/bonces/webservicesbonces/schedule/domain/model/entity/Schedule.java
+++ src/main/java/com/bonces/webservicesbonces/schedule/domain/model/entity/Schedule.java
@@ -1,4 +1,24 @@
 package com.bonces.webservicesbonces.schedule.domain.model.entity;
 
-public class Schedule {
+import com.bonces.webservicesbonces.shared.domain.model.entity.AuditModel;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@With
+@Entity
+@Table(name = "schedules")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Schedule extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
 }
