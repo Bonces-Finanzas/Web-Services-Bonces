5aa151e208b31c9a970876c3d774e1258cf6f8e4
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-05-17 19:21:28 -0500

    implemented user entity

--- src/main/java/com/bonces/webservicesbonces/users/domain/model/entity/User.java
+++ src/main/java/com/bonces/webservicesbonces/users/domain/model/entity/User.java
@@ -1,4 +1,38 @@
 package com.bonces.webservicesbonces.users.domain.model.entity;
 
-public class User {
+import com.bonces.webservicesbonces.shared.domain.model.entity.AuditModel;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@With
+@Entity
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    private String name;
+
+    @NotNull
+    @NotBlank
+    private String lastName;
+
+    @NotNull
+    @NotBlank
+    private String email;
+
+    @NotNull
+    @NotBlank
+    private String password;
 }
