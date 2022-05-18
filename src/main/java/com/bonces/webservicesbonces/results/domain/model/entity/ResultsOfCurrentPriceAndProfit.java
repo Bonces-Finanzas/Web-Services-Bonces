c609d06edc114cc02192f391e6660bbd6cc2fa8e
Alexander Fausto Medina Cortez <alexandermedinacortez@gmail.com>
2022-05-18 11:19:57 -0500

    Entity creation ResultsOfCurrentPriceAndProfit and ResultsOfDecisionRatio

--- src/main/java/com/bonces/webservicesbonces/results/domain/model/entity/ResultsOfCurrentPriceAndProfit.java
+++ src/main/java/com/bonces/webservicesbonces/results/domain/model/entity/ResultsOfCurrentPriceAndProfit.java
@@ -1,4 +1,40 @@
 package com.bonces.webservicesbonces.results.domain.model.entity;
 
-public class ResultsOfCurrentPriceAndProfit {
+import com.bonces.webservicesbonces.schedule.domain.model.entity.Schedule;
+import com.bonces.webservicesbonces.shared.domain.model.entity.AuditModel;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@With
+@Entity
+@Table(name = "resultsOfCurrentPriceAndProfits")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class ResultsOfCurrentPriceAndProfit extends AuditModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Double currentPrice;
+
+    @NotNull
+    private Double lostProfit;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "schedule_id",
+            referencedColumnName = "id",
+            nullable = false
+    )
+    @JsonIgnore
+    private Schedule schedule;
+
 }
--- src/main/java/com/bonces/webservicesbonces/results/domain/model/entity/ResultsOfDecisionRatio.java
+++ src/main/java/com/bonces/webservicesbonces/results/domain/model/entity/ResultsOfDecisionRatio.java
@@ -1,4 +1,44 @@
 package com.bonces.webservicesbonces.results.domain.model.entity;
 
-public class ResultsOfDecisionRatio {
+import com.bonces.webservicesbonces.schedule.domain.model.entity.Schedule;
+import com.bonces.webservicesbonces.shared.domain.model.entity.AuditModel;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@With
+@Entity
+@Table(name = "resultsOfDecisionRatios")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class ResultsOfDecisionRatio extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Double duration;
+
+    @NotNull
+    private Double convexity;
+
+    @NotNull
+    private Double total;
+
+    @NotNull
+    private Double modifiedDuration;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "schedule_id",
+            referencedColumnName = "id",
+            nullable = false
+    )
+    @JsonIgnore
+    private Schedule schedule;
 }
