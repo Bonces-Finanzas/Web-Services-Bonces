a5f4186e7278e00a6b9f0456185319ecf63bb265
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-05-29 10:45:14 -0500

    schedule service implemented

--- src/main/java/com/bonces/webservicesbonces/schedule/domain/service/ScheduleService.java
+++ src/main/java/com/bonces/webservicesbonces/schedule/domain/service/ScheduleService.java
@@ -1,4 +1,13 @@
 package com.bonces.webservicesbonces.schedule.domain.service;
 
+import com.bonces.webservicesbonces.schedule.domain.model.entity.Schedule;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public interface ScheduleService {
+    Schedule getById(Long scheduleId);
+    Schedule create(Long userId, Schedule request);
+    Schedule update(Long scheduleId, Schedule request);
+    ResponseEntity<?> delete(Long scheduleId);
 }
