3c31ec37bd8365dae9fd0e4f57566a6d5faeb9f3
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-05-29 11:08:01 -0500

    quota repository implemented close #30

--- src/main/java/com/bonces/webservicesbonces/quota/domain/persistence/QuotaRepository.java
+++ src/main/java/com/bonces/webservicesbonces/quota/domain/persistence/QuotaRepository.java
@@ -1,4 +1,9 @@
 package com.bonces.webservicesbonces.quota.domain.persistence;
 
-public interface QuotaRepository {
+import com.bonces.webservicesbonces.quota.domain.model.entity.Quota;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuotaRepository extends JpaRepository<Quota, Long> {
 }
