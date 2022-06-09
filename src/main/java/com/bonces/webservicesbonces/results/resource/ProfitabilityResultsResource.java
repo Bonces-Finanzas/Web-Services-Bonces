a7f1517fc6ffe12c477b68ff846f6de2e8a157f9
Sayda <Sayda@DESKTOP-UIN10N4>
2022-06-08 23:15:18 -0500

    Profability

--- src/main/java/com/bonces/webservicesbonces/results/resource/ProfitabilityResultsResource.java
+++ src/main/java/com/bonces/webservicesbonces/results/resource/ProfitabilityResultsResource.java
@@ -1,4 +1,18 @@
 package com.bonces.webservicesbonces.results.resource;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@With
 public class ProfitabilityResultsResource {
+    private Long id;
+    private double emitterTcea;
+    private double emitterTceaWithShield;
+    private double bondHolderTrea;
+    private double emitterTirTcea;
+    private double emitterTirTceaWithShield;
+    private double bondholderTirTrea;
 }
--- src/main/java/com/bonces/webservicesbonces/results/resource/create/CreateProfitabilityResultsResource.java
+++ src/main/java/com/bonces/webservicesbonces/results/resource/create/CreateProfitabilityResultsResource.java
@@ -1,4 +1,30 @@
 package com.bonces.webservicesbonces.results.resource.create;
 
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@With
 public class CreateProfitabilityResultsResource {
+    @NotNull
+    private double emitterTcea;
+
+    @NotNull
+    private double emitterTceaWithShield;
+
+    @NotNull
+    private double bondHolderTrea ;
+
+    @NotNull
+    private double emitterTirTcea;
+
+    @NotNull
+    private double emitterTirTceaWithShield;
+
+    @NotNull
+    private double bondholderTirTrea;
 }
