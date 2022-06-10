bf108139b6536ded00c753d9d630ff2ff056a4f9
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-06-10 11:40:14 -0500

    implemented boundData and InitialCostData mapper

--- src/main/java/com/bonces/webservicesbonces/data/mapping/InitialCostDataMapper.java
+++ src/main/java/com/bonces/webservicesbonces/data/mapping/InitialCostDataMapper.java
@@ -1,4 +1,27 @@
 package com.bonces.webservicesbonces.data.mapping;
 
-public class InitialCostDataMapper {
+import com.bonces.webservicesbonces.data.domain.model.entity.InitialCostData;
+import com.bonces.webservicesbonces.data.resource.InitialCostDataResource;
+import com.bonces.webservicesbonces.data.resource.create.CreateInitialCostDataResource;
+import com.bonces.webservicesbonces.data.resource.update.UpdateInitialCostDataResource;
+import com.bonces.webservicesbonces.shared.mapping.EnhancedModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.Serializable;
+
+public class InitialCostDataMapper implements Serializable {
+    @Autowired
+    private EnhancedModelMapper mapper;
+
+    public InitialCostDataResource toResource(InitialCostData model) {
+        return mapper.map(model, InitialCostDataResource.class);
+    }
+
+    public InitialCostData toModel(CreateInitialCostDataResource resource) {
+        return mapper.map(resource, InitialCostData.class);
+    }
+
+    public InitialCostData toModel(UpdateInitialCostDataResource resource) {
+        return mapper.map(resource, InitialCostData.class);
+    }
 }
