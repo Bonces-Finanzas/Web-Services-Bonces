df259a2b92ad6c1b03da9f18c8ff4cac3e33ce79
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-06-10 11:51:00 -0500

    added results mappers

--- src/main/java/com/bonces/webservicesbonces/results/mapping/ResultsOfCurrentPriceAndProfitMapper.java
+++ src/main/java/com/bonces/webservicesbonces/results/mapping/ResultsOfCurrentPriceAndProfitMapper.java
@@ -1,4 +1,27 @@
 package com.bonces.webservicesbonces.results.mapping;
 
-public class ResultsOfCurrentPriceAndProfitMapper {
+import com.bonces.webservicesbonces.results.domain.model.entity.ResultsOfCurrentPriceAndProfit;
+import com.bonces.webservicesbonces.results.resource.ResultsOfCurrentPriceAndProfitResource;
+import com.bonces.webservicesbonces.results.resource.create.CreateResultsOfCurrentPriceAndProfitResource;
+import com.bonces.webservicesbonces.results.resource.update.UpdateResultsOfCurrentPriceAndProfitResource;
+import com.bonces.webservicesbonces.shared.mapping.EnhancedModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.Serializable;
+
+public class ResultsOfCurrentPriceAndProfitMapper implements Serializable {
+    @Autowired
+    private EnhancedModelMapper mapper;
+
+    public ResultsOfCurrentPriceAndProfitResource toResource(ResultsOfCurrentPriceAndProfit model) {
+        return mapper.map(model, ResultsOfCurrentPriceAndProfitResource.class);
+    }
+
+    public ResultsOfCurrentPriceAndProfit toModel(CreateResultsOfCurrentPriceAndProfitResource resource) {
+        return mapper.map(resource, ResultsOfCurrentPriceAndProfit.class);
+    }
+
+    public ResultsOfCurrentPriceAndProfit toModel(UpdateResultsOfCurrentPriceAndProfitResource resource) {
+        return mapper.map(resource, ResultsOfCurrentPriceAndProfit.class);
+    }
 }
--- src/main/java/com/bonces/webservicesbonces/results/mapping/ResultsOfDecisionRatioMapper.java
+++ src/main/java/com/bonces/webservicesbonces/results/mapping/ResultsOfDecisionRatioMapper.java
@@ -1,4 +1,27 @@
 package com.bonces.webservicesbonces.results.mapping;
 
-public class ResultsOfDecisionRatioMapper {
+import com.bonces.webservicesbonces.results.domain.model.entity.ResultsOfDecisionRatio;
+import com.bonces.webservicesbonces.results.resource.ResultsOfDecisionRatioResource;
+import com.bonces.webservicesbonces.results.resource.create.CreateResultsOfDecisionRatioResource;
+import com.bonces.webservicesbonces.results.resource.update.UpdateResultsOfDecisionRatioResource;
+import com.bonces.webservicesbonces.shared.mapping.EnhancedModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.Serializable;
+
+public class ResultsOfDecisionRatioMapper implements Serializable {
+    @Autowired
+    private EnhancedModelMapper mapper;
+
+    public ResultsOfDecisionRatioResource toResource(ResultsOfDecisionRatio model) {
+        return mapper.map(model, ResultsOfDecisionRatioResource.class);
+    }
+
+    public ResultsOfDecisionRatio toModel(CreateResultsOfDecisionRatioResource resource) {
+        return mapper.map(resource, ResultsOfDecisionRatio.class);
+    }
+
+    public ResultsOfDecisionRatio toModel(UpdateResultsOfDecisionRatioResource resource) {
+        return mapper.map(resource, ResultsOfDecisionRatio.class);
+    }
 }
