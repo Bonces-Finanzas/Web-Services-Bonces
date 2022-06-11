126e1374e6148bea589d6954317391a1a09002ab
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-06-10 20:22:14 -0500

    added new function query for data repositories

--- src/main/java/com/bonces/webservicesbonces/data/domain/persistence/BoundDataRepository.java
+++ src/main/java/com/bonces/webservicesbonces/data/domain/persistence/BoundDataRepository.java
@@ -4,6 +4,9 @@ import com.bonces.webservicesbonces.data.domain.model.entity.BoundData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BoundDataRepository extends JpaRepository<BoundData, Long> {
+    Optional<BoundData> findByScheduleId(Long scheduleId);
 }
