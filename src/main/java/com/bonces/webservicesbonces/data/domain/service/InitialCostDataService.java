725bf0d45ab0fe92c9bca58d20d201a48d511d56
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-06-10 20:22:46 -0500

    implemented data services

--- src/main/java/com/bonces/webservicesbonces/data/domain/service/InitialCostDataService.java
+++ src/main/java/com/bonces/webservicesbonces/data/domain/service/InitialCostDataService.java
@@ -1,4 +1,8 @@
 package com.bonces.webservicesbonces.data.domain.service;
 
+import com.bonces.webservicesbonces.data.domain.model.entity.InitialCostData;
+
 public interface InitialCostDataService {
+    InitialCostData createInitialCostData(Long scheduleId, InitialCostData request);
+    InitialCostData updateInitialCostData(Long scheduleId, InitialCostData request);
 }
